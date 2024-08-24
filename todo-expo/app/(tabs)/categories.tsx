1040d607bf2040e1470fc854141195a160d5ef04
Marius Wörfel <raborogit@gmail.com>
2024-08-24 21:46:59 +0200

    feat(Category): :sparkles: added input with button to add to a list

--- todo-expo/app/(tabs)/categories.tsx
+++ todo-expo/app/(tabs)/categories.tsx
@@ -1,10 +1,30 @@
-import {SafeAreaView, StyleSheet} from 'react-native';
+import {Button, SafeAreaView, StyleSheet, TextInput, View} from 'react-native';
 import {Heading} from "@/components/Heading";
+import {useState} from "react";
+import {ICategory} from "@/data/ICategory";
 
 export default function CategoriesScreen() {
+  const [category, setCategory] = useState<string>("");
+  const [categories, setCategories] = useState<ICategory[]>([]);
+
+  const onAdd = () => {
+      if (category.length <= 0) {
+          return;
+      }
+      setCategories(categories => [...categories, {name: category}]);
+      setCategory("");
+  }
+
   return (
       <SafeAreaView style={styles.container}>
         <Heading name={"Categories"} />
+        <View>
+            <TextInput
+                placeholder={"Add a category"}
+                onChangeText={(category) => setCategory(category)}
+            />
+            <Button title={"Add"} />
+        </View>
       </SafeAreaView>
   );
 }
