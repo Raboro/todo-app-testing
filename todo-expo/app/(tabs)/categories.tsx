cfbdf1657a843d68175f2489245070162c0df599
Marius Wörfel <raborogit@gmail.com>
2024-08-24 21:37:19 +0200

    feat(Category): :sparkles: added styling to heading

--- todo-expo/app/(tabs)/categories.tsx
+++ todo-expo/app/(tabs)/categories.tsx
@@ -1,5 +1,23 @@
-import { Text } from 'react-native';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
 
 export default function CategoriesScreen() {
-  return <Text>Categories</Text>;
+  return (
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.heading}>Categories</Text>
+      </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    height: '100%',
+  },
+  heading: {
+    paddingTop: 10,
+    paddingLeft: 20,
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 50,
+  }
+});
