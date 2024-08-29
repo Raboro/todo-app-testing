2defed93e25191d30f9a308cb688c9af3d3b729d
Raboro <marius.woerfelreal@gmail.com>
2024-08-29 19:14:25 +0200

    feat(TodosList): :sparkles: added red background for dismiss from start to end with delete icon

--- todo-flutter/lib/widgets/todos_list.dart
+++ todo-flutter/lib/widgets/todos_list.dart
@@ -19,6 +19,16 @@ class _TodosListState extends State<TodosList> {
             itemBuilder: (BuildContext context, int index) {
               return Dismissible(
                   key: ValueKey<int>(index),
+                  direction: DismissDirection.startToEnd,
+                  background: Container(
+                    color: Colors.redAccent,
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    alignment: Alignment.centerLeft,
+                    child: const Icon(
+                      Icons.delete,
+                      color: Colors.white,
+                    ),
+                  ),
                   child: Card(
                     child: ListTile(title: Text(todos[index])),
                   ));
