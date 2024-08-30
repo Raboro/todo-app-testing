1ff7c8f4d25648d887ea147298bcc0ff1dcc0725
Raboro <marius.woerfelreal@gmail.com>
2024-08-31 01:01:34 +0200

    refactor(Main): :recycle: renamed main widget to TodoApp

--- todo-flutter/test/widget_test.dart
+++ todo-flutter/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:todo_flutter/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const Todo());
+    await tester.pumpWidget(const TodoApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
