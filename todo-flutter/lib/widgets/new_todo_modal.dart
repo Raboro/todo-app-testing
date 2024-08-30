122bddc2b051b340cac4339edbbb13267cf4110b
Raboro <marius.woerfelreal@gmail.com>
2024-08-31 01:42:34 +0200

    refactor(NewTodoModal): :recycle: made component stateful, saved input field values in controllers, created new todo and added to provider

--- todo-flutter/lib/widgets/new_todo_modal.dart
+++ todo-flutter/lib/widgets/new_todo_modal.dart
@@ -1,13 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_flutter/data/todo_model.dart';
+import 'package:todo_flutter/provider/todos_provider.dart';
 import 'package:todo_flutter/widgets/heading.dart';
 
-class NewTodoModal extends StatelessWidget {
+class NewTodoModal extends StatefulWidget {
   const NewTodoModal({super.key});
 
-  Widget _buildInputField({required String hintText}) {
+  @override
+  State<StatefulWidget> createState() => _NewTodoModalState();
+}
+
+class _NewTodoModalState extends State<NewTodoModal> {
+  final TextEditingController _todoNameController = TextEditingController();
+  final TextEditingController _todoDescriptionController =
+      TextEditingController();
+
+  @override
+  void dispose() {
+    _todoNameController.dispose();
+    _todoDescriptionController.dispose();
+    super.dispose();
+  }
+
+  Widget _buildInputField(
+      {required String hintText, required TextEditingController controller}) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: TextField(
+        controller: controller,
         decoration: InputDecoration(
             border: const OutlineInputBorder(), hintText: hintText),
       ),
@@ -24,12 +45,23 @@ class NewTodoModal extends StatelessWidget {
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
           const Heading(text: 'Add new Todo'),
-          _buildInputField(hintText: 'Enter a new Todo'),
-          _buildInputField(hintText: 'Description of Todo'),
+          _buildInputField(
+              hintText: 'Enter a new Todo', controller: _todoNameController),
+          _buildInputField(
+              hintText: 'Description of Todo',
+              controller: _todoDescriptionController),
           Row(
             children: <ButtonStyleButton>[
               ElevatedButton(
                   onPressed: () {
+                    String? description = _todoDescriptionController.text;
+                    if (description.isEmpty) {
+                      description = null;
+                    }
+                    TodoModel todo = TodoModel(
+                        name: _todoNameController.text,
+                        description: description);
+                    context.read<TodosProvider>().addTodo(todo: todo);
                     Navigator.pop(context);
                   },
                   child: const Text('Add')),
