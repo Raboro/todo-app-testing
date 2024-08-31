f9d8c95498f016a198745936daf468051aad367d
Raboro <marius.woerfelreal@gmail.com>
2024-08-31 23:12:51 +0200

    feat(CategoriesContainer): :sparkles: used provider to get list of categories

--- todo-flutter/lib/widgets/categories_container.dart
+++ todo-flutter/lib/widgets/categories_container.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:todo_flutter/data/category_model.dart';
+import 'package:todo_flutter/provider/categories_provider.dart';
 
 class CategoriesContainer extends StatefulWidget {
   const CategoriesContainer({super.key});
@@ -9,21 +11,11 @@ class CategoriesContainer extends StatefulWidget {
 }
 
 class _CategoriesContainerState extends State<CategoriesContainer> {
-  final List<CategoryModel> categories = <CategoryModel>[
-    'Technology',
-    'Science',
-    'Sports',
-    'Music',
-    'Art',
-    'Travel',
-    'Movies',
-    'Books',
-    'Fitness',
-    'Cooking'
-  ];
+  List<CategoryModel> categories = [];
 
   @override
   Widget build(BuildContext context) {
+    categories = context.watch<CategoriesProvider>().categories;
     return Padding(
       padding: const EdgeInsets.all(15.0),
       child: Wrap(
