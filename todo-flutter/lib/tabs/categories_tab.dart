60532ff3a8ecbc629e48177f771cc025e80811be
Raboro <marius.woerfelreal@gmail.com>
2024-08-31 15:01:02 +0200

    feat(Category): :sparkles: used add new category widget in column

--- todo-flutter/lib/tabs/categories_tab.dart
+++ todo-flutter/lib/tabs/categories_tab.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:todo_flutter/widgets/heading.dart';
 
+import '../widgets/add_new_category.dart';
+
 class CategoriesTab extends StatelessWidget {
   const CategoriesTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Heading(text: 'Categories');
+    return const Column(
+      children: [Center(child: Heading(text: 'Categories')), AddNewCategory()],
+    );
   }
 }
