c7ba6136dd60dec2cc1bc5860d997f2c085a85c6
Raboro <marius.woerfelreal@gmail.com>
2024-09-01 15:36:45 +0200

    feat(Main): :sparkles: included & used LocaleProvider in MaterialApp

--- todo-flutter/lib/main.dart
+++ todo-flutter/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_flutter/provider/categories_provider.dart';
+import 'package:todo_flutter/provider/locale_provider.dart';
 import 'package:todo_flutter/provider/todos_provider.dart';
 import 'package:todo_flutter/tabs/tabs.dart';
 
@@ -10,25 +11,36 @@ void main() {
   runApp(const TodoApp());
 }
 
-class TodoApp extends StatelessWidget {
+class TodoApp extends StatefulWidget {
   const TodoApp({super.key});
 
+  @override
+  State<StatefulWidget> createState() => TodoAppState();
+}
+
+class TodoAppState extends State<TodoApp> {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (context) => TodosProvider()),
         ChangeNotifierProvider(create: (context) => CategoriesProvider()),
+        ChangeNotifierProvider(create: (context) => LocaleProvider()),
       ],
-      child: MaterialApp(
-        title: 'Todo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        localizationsDelegates: AppLocalizations.localizationsDelegates,
-        supportedLocales: AppLocalizations.supportedLocales,
-        home: const Tabs(),
+      child: Builder(
+        builder: (context) {
+          return MaterialApp(
+            title: 'Todo',
+            theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              useMaterial3: true,
+            ),
+            locale: context.watch<LocaleProvider>().locale,
+            localizationsDelegates: AppLocalizations.localizationsDelegates,
+            supportedLocales: AppLocalizations.supportedLocales,
+            home: const Tabs(),
+          );
+        },
       ),
     );
   }
