afbd03dce3d9eaeca7e3702b7f330ebcc3df8903
Raboro <marius.woerfelreal@gmail.com>
2024-09-01 16:25:37 +0200

    feat(Settings): :sparkles: enhanced with DropdownMenu to change language

--- todo-flutter/lib/tabs/settings_tabs.dart
+++ todo-flutter/lib/tabs/settings_tabs.dart
@@ -1,15 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_flutter/provider/locale_provider.dart';
 import 'package:todo_flutter/widgets/heading.dart';
 
-class SettingsTab extends StatelessWidget {
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class SettingsTab extends StatefulWidget {
   const SettingsTab({super.key});
 
   @override
+  State<StatefulWidget> createState() => SettingsTabState();
+}
+
+class SettingsTabState extends State<SettingsTab> {
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Column(children: [
-        Heading(text: 'Settings'),
-      ]),
+    return Center(
+      child: Column(
+        children: [
+          const Heading(text: 'Settings'),
+          DropdownMenu<String>(
+            initialSelection: context.watch<LocaleProvider>().locale.toString(),
+            onSelected: (String? selected) {
+              if (selected == null) {
+                return;
+              }
+              context
+                  .read<LocaleProvider>()
+                  .changeLocalTo(locale: Locale(selected));
+            },
+            dropdownMenuEntries: AppLocalizations.supportedLocales
+                .map(
+                  (locale) => DropdownMenuEntry<String>(
+                    value: locale.toString(),
+                    label: locale.toString(),
+                  ),
+                )
+                .toList(),
+          )
+        ],
+      ),
     );
   }
 }
