2a278505897d268c926dce7a1489f5084966ac93
Marius Wörfel <marius.woerfelreal@gmail.com>
2024-09-03 23:33:05 +0200

    style(Biome): :art: used biome to format code

--- todo-expo/components/Todo.spec.tsx
+++ todo-expo/components/Todo.spec.tsx
@@ -1,12 +1,16 @@
-import { describe, it, expect } from '@jest/globals'
-import { render } from '@testing-library/react-native'
+import type { ITodo } from '@/data/ITodo';
+import { describe, expect, it } from '@jest/globals';
+import { render } from '@testing-library/react-native';
 import { Todo } from './Todo';
-import { ITodo } from '@/data/ITodo';
 
 describe('Todo Tests', () => {
-
-    it('Todo component should be rendered correctly', () => {
-        const todo = render(<Todo todo={{name: 'Homework', description: 'Do your homework'}} onDelete={(todo: ITodo) => {}} />);
-        expect(todo).not.toBeNull();
-    });
+  it('Todo component should be rendered correctly', () => {
+    const todo = render(
+      <Todo
+        todo={{ name: 'Homework', description: 'Do your homework' }}
+        onDelete={(todo: ITodo) => {}}
+      />,
+    );
+    expect(todo).not.toBeNull();
+  });
 });
