c538eb4e3bc56075e6e673fbb6f1d36af273edd6
Marius Wörfel <marius.woerfelreal@gmail.com>
2024-09-06 22:44:00 +0200

    feat(i18n): :sparkles: added i18n config

---
+++ todo-expo/i18n/i18n.js
@@ -0,0 +1,43 @@
+import 'intl-pluralrules';
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';  // Stelle sicher, dass das korrekt importiert ist
+import * as Localization from 'expo-localization';
+import translationEN from './locales/en.json';
+import translationDE from './locales/de.json';
+
+const resources = {
+  en: {
+    translation: translationEN,
+  },
+  de: {
+    translation: translationDE,
+  },
+};
+
+const languageDetector = {
+  type: 'languageDetector',
+  async: true,
+  detect: callback => {
+    const bestLanguage = Localization.getLocales()[0].languageTag;
+    callback(bestLanguage);
+  },
+  init: () => { },
+  cacheUserLanguage: () => { },
+};
+
+// Init i18n
+i18n
+  .use(languageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+    react: {
+      useSuspense: false,
+    },
+  });
+
+export default i18n;
